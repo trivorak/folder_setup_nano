b8285920b6f5b6823cb41d4824c2a207416b0bf3
Drake Champagne <trivorak@gmail.com>
2017-03-02 08:42:40 -0600

    Added a copy command to copy and excel file from the source folder this is specific to my project

--- Automate.bat
+++ Automate.bat
@@ -1,3 +1,4 @@
 MD "Signed Inspection Reports" Backup "CMM Results - PDF"
 copy NUL "Drawing Suggestions.txt"
+copy "C:\Users\dchampagne\Desktop\Nanosphere\source\01776 App1.xlsm" "01776 App1.xlsm"
 del Automate.bat
