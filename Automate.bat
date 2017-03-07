165f176ce164b883e1ad45915f6daad4a50c2116
Drake Champagne <trivorak@gmail.com>
2017-03-07 10:28:12 -0600

    Added  another file copy line

--- Automate.bat
+++ Automate.bat
@@ -1,4 +1,5 @@
 MD "Signed Inspection Reports" Backup "CMM Results - PDF"
 copy NUL "Drawing Suggestions.txt"
 copy "C:\Users\dchampagne\Desktop\Nanosphere\source\01776 App1.xlsm" "01776 App1.xlsm"
+copy "C:\Users\dchampagne\Desktop\Nanosphere\source\Fill Me Out.xlsx" "Fill Me Out.xlsx"
 del Automate.bat
