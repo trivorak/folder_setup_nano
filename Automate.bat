596454309bcdc93e90c32ac2aadba1f9710011b0
Drake Champagne <trivorak@gmail.com>
2017-04-12 12:12:28 -0500

    Removed some of the instructions. Those instructions are not needed currently

--- Automate.bat
+++ Automate.bat
@@ -1,5 +1,4 @@
-MD "Signed Inspection Reports" Backup "CMM Results - PDF"
-copy NUL "Drawing Suggestions.txt"
+MD "Signed Inspection Reports"
 copy "C:\Users\dchampagne\Desktop\Nanosphere\source\01776 App1.xlsm" "01776 App1.xlsm"
 copy "C:\Users\dchampagne\Desktop\Nanosphere\source\Fill Me Out.xlsx" "Fill Me Out.xlsx"
 del Automate.bat
